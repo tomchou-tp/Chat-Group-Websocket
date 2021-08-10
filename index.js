5f18783388281586dd3852031902f56c3924207e
ChiHsien Chou <chougitom@gmail.com>
2021-08-10 16:02:22 +0800

    1. Add response from server. 2. Remove unsed code. 3. Correct variable names. 4. Add automatic reconnection.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const webSocket = require('ws');
-const express = require('express');
-const https = require('https');
-const fs = require('fs');
+const webSocket = require("ws");
+const express = require("express");
+const http = require("http");
 var room_info = {};
 
 function usr_info(usr_id, ws, cordn_sync_ctl) {
@@ -11,39 +10,78 @@ function usr_info(usr_id, ws, cordn_sync_ctl) {
 }
 
 function rcv_msg(msg, ws) {
+    let out_msg;
     msg = JSON.parse(msg);
     switch (msg.func_id) {
-        case "conn":
-            if (room_info[msg.room_id] === undefined) {
-                room_info[msg.room_id] = [
-                    new usr_info(msg.usr_id, ws, false)
-                ];
-            } else {
+        case "create":
+            room_info[msg.room_id] = [
+                new usr_info(msg.usr_id, ws, false)
+            ];
+            out_msg = {
+                func_id: "create_res",
+                code: 0
+            };
+            ws.send(JSON.stringify(out_msg));
+            break;
+        case "join":
+            if (room_info[msg.room_id] !== undefined) {
+                let is_new_user = true;
                 for (let i = 0; i < room_info[msg.room_id].length; i++) {
-                    if (msg.usr_id == room_info[msg.room_id][i]['usr_id']) {
+                    if (msg.usr_id == room_info[msg.room_id][i]["usr_id"]) {
                         if (room_info[msg.room_id][i].ws != ws) {
-                            room_info[msg.room_id][i].ws.close();
+                            room_info[msg.room_id][i].ws.close(4000);
                             room_info[msg.room_id][i].ws = ws;
                         }
-                        return;
+                        is_new_user = false;
+                        break;
                     }
                 }
-                room_info[msg.room_id].push(
-                    new usr_info(msg.usr_id, ws, true)
-                );
+                if (is_new_user)
+                    room_info[msg.room_id].push(
+                        new usr_info(msg.usr_id, ws, true)
+                    );
+                out_msg = {
+                    func_id: "join_res",
+                    code: 0
+                };
+                ws.send(JSON.stringify(out_msg));
+            } else {
+                out_msg = {
+                    func_id: "join_res",
+                    code: 1
+                };
+                ws.send(JSON.stringify(out_msg));
             }
             break;
         case "msg":
-            if (room_info[msg.room_id] === undefined) break;
+            out_msg = {};
+            if (room_info[msg.room_id] === undefined) {
+                out_msg = {
+                    func_id: "msg_res",
+                    code: 1
+                };
+                ws.send(JSON.stringify(out_msg));
+                break;
+            }
+            msg.func_id = "msg_res";
+            msg.code = 0;
             out_msg = JSON.stringify(msg);
             for (let i = 0; i < room_info[msg.room_id].length; i++) {
                 room_info[msg.room_id][i]["ws"].send(out_msg);
             }
             break;
         case "cordn":
-            if (room_info[msg.room_id] === undefined) break;
+            if (room_info[msg.room_id] === undefined) {                
+                out_msg = {
+                    func_id: "cordn_res",
+                    code: 1
+                };
+                ws.send(JSON.stringify(out_msg));
+            };
             if (msg["usr_id"] == room_info[msg.room_id][0]["usr_id"]) {
                 delete msg.usr_id;
+                msg.func_id = "cordn_res";
+                msg.code = 0;
                 out_msg = JSON.stringify(msg);
                 for (let i = 1; i < room_info[msg.room_id].length; i++) {
                     if (room_info[msg.room_id][i]["cordn_sync_ctl"]) {
@@ -57,40 +95,37 @@ function rcv_msg(msg, ws) {
     }
 }
 
-const wss = new webSocket.Server({ noServer: true });
-wss.on('connection', (ws) => {
-    ws.on('message', (msg) => {
+const ws_serv = new webSocket.Server({ noServer: true });
+ws_serv.on("connection", (ws) => {
+    ws.on("message", (msg) => {
         rcv_msg(msg, ws);
     }); 
 });
 
 const app = express();
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-const serv = https.createServer({
-    key: fs.readFileSync("./cert/demo_key"),
-    cert: fs.readFileSync("./cert/demo_cert")
-}, app);
+const serv = http.createServer(app);
 
 serv.listen(process.env.PORT || 8443, function() {
-    console.log('Web server is running.');
+    console.log("Web server is running.");
 });
 
-serv.on('upgrade', (request, socket, head) => {
-    wss.handleUpgrade(request, socket, head, socket => {
-        wss.emit('connection', socket, request);
+serv.on("upgrade", (request, socket, head) => {
+    ws_serv.handleUpgrade(request, socket, head, socket => {
+        ws_serv.emit("connection", socket, request);
     });
   }
 );
 
-app.post('/cordn_sync_ctl', function(req, res) {
+app.post("/cordn_sync_ctl", function(req, res) {
     if (room_info[req.body.room_id] !== undefined &&
-        room_info[req.body.room_id][0]['usr_id'] != req.body.usr_id
+        room_info[req.body.room_id][0]["usr_id"] != req.body.usr_id
     ) {
         for (let i = 0; i < room_info[req.body.room_id].length; i++) {
-            if (req.body.usr_id == room_info[req.body.room_id][i]['usr_id']) {
-                room_info[req.body.room_id][i]['cordn_sync_ctl'] = req.body.cordn_sync_ctl;
+            if (req.body.usr_id == room_info[req.body.room_id][i]["usr_id"]) {
+                room_info[req.body.room_id][i]["cordn_sync_ctl"] = req.body.cordn_sync_ctl;
                 break;
             }
         }
@@ -98,10 +133,10 @@ app.post('/cordn_sync_ctl', function(req, res) {
     res.send();
 });
 
-app.post('/reset', function(req, res) {
-    for (let i = 0; i < Object.keys(room_info); i++) {
-        for (let j = 0; i < room_info[i].length; i++) {
-            room_info[i][j]['ws'].close();
+app.post("/reset", function(req, res) {
+    for (let i = 0; i < Object.keys(room_info).length; i++) {
+        for (let j = 0; j < room_info[Object.keys(room_info)[i]].length; j++) {
+            room_info[Object.keys(room_info)[i]][j]["ws"].close(4000);
         }
     }
     room_info = {};
